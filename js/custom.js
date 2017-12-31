3a4607656bed489cfe48cfde7457e7108729f060
pit <orel-1993@yandex.com>
2017-12-31 12:59:07 +0200

    first Project

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+$(function(){
+	var hf = function(){
+		var h_header = $('header').height();
+		var h_footer = $('footer').height();
+		$('.content').css({
+			'paddingTop': h_header,
+			'paddingBottom': h_footer
+		});
+
+	}
+
+	$(window).bind('load resize', hf);
+
+});
+
+   
+
+
