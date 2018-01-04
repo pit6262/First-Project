b090afbae2a57d2cc6d2c205089bf17bdea8e742
pit <orel-1993@yandex.com>
2018-01-04 20:12:03 +0200

    ver1.0

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,5 @@
 $(function(){
-	var hf = function(){
-		var h_header = $('header').height();
-		var h_footer = $('footer').height();
-		$('.content').css({
-			'paddingTop': h_header,
-			'paddingBottom': h_footer
-		});
 
-	}
-
-	$(window).bind('load resize', hf);
 
 });
 
